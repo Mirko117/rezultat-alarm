514d68d87dc4d66fff04a35687e4a17854e9b85b
Mirko <suvajac117@gmail.com>
2026-04-23 14:59:54 +0200

    Add more logs and improve scraper validation

--- apps/core/scraper.py
+++ apps/core/scraper.py
@@ -17,6 +17,8 @@ def scrape_from_major(major: Major):
 
     html = response.text
 
+    # Calculate the hash of the page content to detect changes
+    # If hash is the same as the last snapshot, that means that the page has not changed and we skip
     page_hash = hashlib.sha256(response.content).hexdigest()
     last_snapshot: PageSnapshot = major.snapshots.order_by("-fetched_at").first()  # type: ignore
 
@@ -34,6 +36,10 @@ def scrape_from_major(major: Major):
 
     school_classes = soup.find_all("a", id=re.compile(r"^PrTAG\d+$"))
 
+    if not school_classes:
+        logger.info(f"No classes found for {major.name}.")
+        return
+
     for school_class in school_classes:
         # Extract class name
         class_name = school_class.find_next("h2")
@@ -49,9 +55,11 @@ def scrape_from_major(major: Major):
         else:
             professor_full_name = "Neznan profesor"
 
-        ## Check against the database
+        # Check against the database
 
-        professor, _ = Professor.objects.get_or_create(full_name=professor_full_name)
+        professor, created = Professor.objects.get_or_create(full_name=professor_full_name)
+        if created:
+            logger.debug(f"New professor found: {professor_full_name}. Added to the database.")
 
         # class_ because class is a reserved keyword in Python
         class_ = SchoolClass.objects.filter(name=class_name, major=major).first()
@@ -59,119 +67,164 @@ def scrape_from_major(major: Major):
             if class_.professor != professor:
                 class_.professor = professor
                 class_.save()
+                logger.debug(
+                    f"Professor updated for {class_name}."
+                    f" {class_.professor.full_name} -> {professor.full_name}"
+                )
         else:
             class_ = SchoolClass.objects.create(name=class_name, major=major, professor=professor)
+            logger.debug(f"New class found: {class_name}. Added to the database.")
 
         table = school_class.find_next("table")
-        if table:
-            rows = table.find_all("tr")
+        if not table:
+            logger.warning(f"Table not found for {class_name}. Skipping.")
+            continue
 
-            table_length = len(rows)
-            if table_length < 2:
-                continue  # Skip tables that don't have exams
+        rows = table.find_all("tr")
 
-            # First row is the header, skip it
-            for i in range(1, len(rows)):
-                row = rows[i]
-                cells = row.find_all("td")
-                if len(cells) == 4:
-                    exam_name = cells[0].text.strip()
-                    exam_date = cells[1].text.strip()
-                    exam_time_and_classroom = cells[2].text.strip()
+        table_length = len(rows)
+        if table_length < 2:
+            logger.debug(f"Table for {class_name} is empty. Skipping.")
+            continue  # Skip tables that don't have exams
 
-                    exam_code = cells[3].find_next("a")
-                    if exam_code:
-                        # RezultatiRoka17393.htm -> 17393
-                        exam_code = str(exam_code["href"])
-                        exam_code = re.search(r"\d+", exam_code)
-                        exam_code = exam_code.group(0) if exam_code else None
+        # First row is the header, skip it
+        for i in range(1, len(rows)):
+            row = rows[i]
+            cells = row.find_all("td")
+            if len(cells) != 4:
+                logger.error(f"Unexpected number of cells in row for {class_name}. Skipping.")
+                continue
 
-                    if not exam_code:
-                        logger.warning(
-                            f"Warning: No exam code found for {exam_name} in {class_name}."
-                            " Skipping."
-                        )
-                        continue
+            # Extract exam data
+            exam_name = cells[0].text.strip()
+            exam_date = cells[1].text.strip()
+            exam_time_and_classroom = cells[2].text.strip()
+            exam_results = cells[3].find_next("img")
+            exam_code = cells[3].find_next("a")
 
-                        # Right now we are skipping exams without code because we have no way to
-                        # identify them in the database and they are not that important anyway,
-                        # but maybe in the future we can add unique constraint to the records with
-                        # no code and identify them by their name, date and time or something like
-                        # that
+            # First do exam_code ckecks because code is mandatory
 
-                    # Check if the exam has results available
-                    exam_results = cells[3].find_next("img")
-                    if exam_results:
-                        exam_results = exam_results["src"] == "EvidencaRez.gif"
-                    else:
-                        exam_results = False
+            if not exam_code:
+                logger.warning(f"No exam code found for {exam_name} in {class_name}. Skipping.")
+                continue
+                # Right now we are skipping exams without code because we have no way to
+                # identify them in the database and they are not that important anyway,
+                # but maybe in the future we can add unique constraint to the records with
+                # no code and identify them by their name, date and time or something like
+                # that
 
-                    exam_time_and_classroom = exam_time_and_classroom.split(", ")
+            # RezultatiRoka17393.htm -> 17393
+            exam_code = str(exam_code["href"])
+            exam_code = re.search(r"\d+", exam_code)
+            if exam_code:
+                exam_code = exam_code.group(0)
+            else:
+                logger.warning(
+                    f"Could not extract exam code for {exam_name} in {class_name}. Skipping."
+                )
+                continue
 
-                    exam_time = ""
-                    exam_classroom = ""
+            if not exam_name:
+                exam_name = "Neznan izpit"
+                logger.warning(
+                    f"No exam name found for exam code {exam_code} in {class_name}."
+                    " Using default name."
+                )
 
-                    if len(exam_time_and_classroom) == 1:
-                        exam_time = exam_time_and_classroom[0]
-                        exam_classroom = ""
-                    elif len(exam_time_and_classroom) >= 2:
-                        exam_time = exam_time_and_classroom[0]
-                        exam_classroom = ", ".join(exam_time_and_classroom[1:])
+            if exam_date != "" and exam_date is not None:
+                # Convert to ISO format
+                # Check if date has space after dot
+                split_symbol = ". " if ". " in exam_date else "."
 
-                    ## Process extracted data
+                # Split it and reverse it to get it in the correct order for ISO format (YYYY-MM-DD)
+                # 6. 1. 2026 -> ["2026", "1", "6"]
+                exam_date = exam_date.split(split_symbol)[::-1]
 
-                    if exam_date != "":
-                        # Convert to ISO format
-                        exam_date = exam_date.split(". ")[::-1]
-                        for i in range(len(exam_date)):
-                            exam_date[i] = exam_date[i].zfill(2)
-                        exam_date = "-".join(exam_date)
-                        exam_date = date.fromisoformat(exam_date)
-                    else:
-                        exam_date = None
+                # Add leading zeros if needed
+                for i in range(len(exam_date)):
+                    exam_date[i] = exam_date[i].zfill(2)
 
-                    if exam_time != "":
-                        # Convert to ISO format
-                        exam_time = exam_time.replace(".", ":")
-                        exam_time = time.fromisoformat(exam_time)
-                    else:
-                        exam_time = None
+                # Join it back together
+                exam_date = "-".join(exam_date)
 
-                    # TODO: Maybe add additional regex check to ensure the time and date are in the
-                    #       correct format just to be safe
+                # Validate the date format using regex
+                is_valid_date_format = re.match(r"\d{4}-\d{2}-\d{2}", exam_date)
 
-                    # Check if the exam already exists in the database
-                    exams = class_.exams.order_by("-date", "-time")  # type: ignore
+                if not is_valid_date_format:
+                    logger.warning(
+                        f"Exam date {exam_date} for {exam_name} in {class_name} is not in the"
+                        " correct format. Setting to None."
+                    )
+                    exam_date = None
+                else:
+                    # Convert to date object
+                    exam_date = date.fromisoformat(exam_date)
+            else:
+                exam_date = None
 
-                    if not exams.filter(code=exam_code).exists():
-                        Exam.objects.create(
-                            name=exam_name,
-                            code=exam_code,
-                            date=exam_date,
-                            time=exam_time,
-                            classroom=exam_classroom,
-                            results_available=exam_results,
-                            school_class=class_,
-                        )
-                    else:
-                        # Check is there are any differences
-                        current_exam = exams.filter(code=exam_code).first()
+            # Same as above
+            # Just this time it splits it in time and classroom
+            # Example from site: 14.45, 352 Učilnica
+            split_symbol = ", " if ", " in exam_time_and_classroom else ","
+            exam_time_and_classroom = exam_time_and_classroom.split(split_symbol)
 
-                        if (
-                            current_exam.name != exam_name
-                            or current_exam.date != exam_date
-                            or current_exam.time != exam_time
-                            or current_exam.classroom != exam_classroom
-                            or current_exam.results_available != exam_results
-                        ):
-                            current_exam.name = exam_name
-                            current_exam.date = exam_date
-                            current_exam.time = exam_time
-                            current_exam.classroom = exam_classroom
-                            current_exam.results_available = exam_results
-                            current_exam.save()
+            # TODO: Finish further validation from here to the end
 
-                            logger.info(f"Exam {exam_name} in {class_name} has been updated.")
+            exam_time = ""
+            exam_classroom = ""
 
-                            # TODO: Send Email about the change
-                            # TODO: Maybe send email to admin if error occurs
+            if len(exam_time_and_classroom) == 1:
+                exam_time = exam_time_and_classroom[0]
+                exam_classroom = ""
+            elif len(exam_time_and_classroom) >= 2:
+                exam_time = exam_time_and_classroom[0]
+                exam_classroom = ", ".join(exam_time_and_classroom[1:])
+
+            if exam_time != "":
+                # Convert to ISO format
+                exam_time = exam_time.replace(".", ":")
+                exam_time = time.fromisoformat(exam_time)
+            else:
+                exam_time = None
+
+            # Check if the exam has results available
+            if exam_results:
+                exam_results = exam_results["src"] == "EvidencaRez.gif"
+            else:
+                exam_results = False
+
+            # Check if the exam already exists in the database
+            exams = class_.exams.order_by("-date", "-time")  # type: ignore
+
+            if not exams.filter(code=exam_code).exists():
+                Exam.objects.create(
+                    name=exam_name,
+                    code=exam_code,
+                    date=exam_date,
+                    time=exam_time,
+                    classroom=exam_classroom,
+                    results_available=exam_results,
+                    school_class=class_,
+                )
+            else:
+                # Check is there are any differences
+                current_exam = exams.filter(code=exam_code).first()
+
+                if (
+                    current_exam.name != exam_name
+                    or current_exam.date != exam_date
+                    or current_exam.time != exam_time
+                    or current_exam.classroom != exam_classroom
+                    or current_exam.results_available != exam_results
+                ):
+                    current_exam.name = exam_name
+                    current_exam.date = exam_date
+                    current_exam.time = exam_time
+                    current_exam.classroom = exam_classroom
+                    current_exam.results_available = exam_results
+                    current_exam.save()
+
+                    logger.info(f"Exam {exam_name} in {class_name} has been updated.")
+
+                    # TODO: Send Email about the change
+                    # TODO: Maybe send email to admin if error occurs
